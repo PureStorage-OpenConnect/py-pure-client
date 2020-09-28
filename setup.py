aeb1327f7d6b2a3c8f193172777caa80222ddf1c
Yuehan Xu <yxu@purestorage.com>
2020-09-28 10:51:49 -0700

    Update paramiko version to fit python2/3 environment

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ NAME = 'py-pure-client'
 VERSION = '1.8.0'
 
 REQUIRES = ['urllib3 >= 1.15', 'six >= 1.10', 'certifi >= 14.05.14',
-            'python-dateutil >= 2.5.3', 'paramiko >= 2.4.2',
+            'python-dateutil >= 2.5.3', 'paramiko == 2.7.1',
             'PyJWT >= 1.7.1', 'requests >= 2.20.1']
 
 readme = open('README.md', 'r')
